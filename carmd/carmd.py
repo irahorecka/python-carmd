caaffabffba45da3cb49c2572141a856d234645a
Ira Horecka <ira89@icloud.com>
2020-03-27 01:40:20 -0700

    modify carmd.py to accept early vehicle instances

--- carmd/carmd.py
+++ carmd/carmd.py
@@ -2,37 +2,6 @@ import json
 import requests
 
 
-class BaseAPI():
-    """Base wraper for individual CarMD requests."""
-    base_url = 'http://api.carmd.com/v3.0'
-    api_header = {
-        "content-type": "application/json",
-        "authorization": "your_auth_key_here",
-        "partner-token": "your_partner_token_here"
-    }
-
-    def __init__(self, auth, partner):
-        """Initialize with appropriate authorization and partner-token for API header"""
-        if not auth:
-            raise KeyError("Please pass a valid authorization key: "
-                           "e.g. 'Basic MmE0MzBkZjYtOTIxOS00ODhjLTllMjktNjQ2MDlhMmY1OWZx'")
-        if not partner:
-            raise KeyError("Please pass a valid partner-token key: "
-                           "e.g. 'e18dc0f62dfb456398c83b893d3e81e2'")
-        self.api_header["authorization"] = auth
-        self.api_header["partner-token"] = partner
-
-    def get(self, endpoint):
-        """Get request from specified url endpoint."""
-        url = '{}/{}'.format(self.base_url, endpoint)
-        response = requests.get(url, headers=self.api_header)
-        if response.status_code != 200:
-            raise ConnectionError("Bad request: {}".format(response.status_code))
-        response_json = json.loads(response.content)
-
-        return response_json
-
-
 def api_method(method):
     """Decorator for using method signatures for generating url endpoint."""
     def wrapper(self, *args, **kwargs):
@@ -45,45 +14,72 @@ def api_method(method):
         else:
             endpoint = '{}?{}'.format(api, vehicle_id)
         response = self.get(endpoint)
-        
+
         return response
 
     return wrapper
 
 
-class Fields(BaseAPI):
-    """API for available fields:
+class BaseAPI():
+    """Base wraper for individual CarMD requests."""
+    base_url = 'http://api.carmd.com/v3.0'
+    api_header = {
+        "content-type": "application/json",
+        "authorization": "your_auth_key_here",
+        "partner-token": "your_partner_token_here"
+    }
+
+    def __init__(self, auth, partner, **kwargs):
+        """Initialize with appropriate authorization and partner-token for API header"""
+        if not auth:
+            raise KeyError("Please pass a valid authorization key: "
+                           "e.g. 'Basic MmE0MzBkZjYtOTIxOS00ODhjLTllMjktNjQ2MDlhMmY1OWZx'")
+        if not partner:
+            raise KeyError("Please pass a valid partner-token key: "
+                           "e.g. 'e18dc0f62dfb456398c83b893d3e81e2'")
+        self.api_header["authorization"] = auth
+        self.api_header["partner-token"] = partner
+        self.kwargs = kwargs
+
+    def get(self, endpoint):
+        """Get request from specified url endpoint."""
+        url = '{}/{}'.format(self.base_url, endpoint)
+        response = requests.get(url, headers=self.api_header)
+        if response.status_code != 200:
+            raise ConnectionError("Bad request: {}".format(response.status_code))
+        response_json = json.loads(response.content)
+
+        return response_json
+
+
+class FieldsVin(BaseAPI):
+    """API for available fields under VIN:
     http://api.carmd.com/v3.0/fields"""
     api = 'fields'
-
     @api_method
-    def vin(self, vin_no, mileage=None, dtc=None):
-        self.vehicle = {
-            'vin': vin_no,
-            'mileage': mileage,
-            'dtc': dtc
-        }
+    def __call__(self, mileage=None, dtc=None):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
+        self.vehicle['dtc'] = dtc
 
+
+class FieldsMake(BaseAPI):
+    """API for available fields under make:
+    http://api.carmd.com/v3.0/fields"""
+    api = 'fields'
     @api_method
-    def make(self, year, manufacturer, model, mileage=None):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper(),
-            'mileage': mileage
-        }
+    def __call__(self, mileage=None):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
 
 
 class Decode(BaseAPI):
     """API for VIN decode:
     http://api.carmd.com/v3.0/decode"""
     api = 'decode'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class DecodeEnhanced(BaseAPI):
@@ -99,185 +95,106 @@ class OBDPortLocation(BaseAPI):
     """API for finding OBD2 port location:
     http://api.carmd.com/v3.0/port"""
     api = 'port'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper()
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class Maintenance(BaseAPI):
     """API for maintenance:
     http://api.carmd.com/v3.0/maint"""
     api = 'maint'
-
     @api_method
-    def vin(self, vin_no, mileage):
-        self.vehicle = {
-            'vin': vin_no,
-            'mileage': mileage
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model, mileage):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper(),
-            'mileage': mileage
-        }
+    def __call__(self, mileage):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
 
 
 class MaintenanceList(BaseAPI):
     """API for full maintenance list:
     http://api.carmd.com/v3.0/maintlist"""
     api = 'maintlist'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper()
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class Repairs(BaseAPI):
     """API for repair information:
     http://api.carmd.com/v3.0/repair"""
     api = 'repair'
-
     @api_method
-    def vin(self, vin_no, mileage, dtc):
-        self.vehicle = {
-            'vin': vin_no,
-            'mileage': mileage,
-            'dtc': dtc
-        }
+    def __call__(self, mileage, dtc):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
+        self.vehicle['dtc'] = dtc
 
 
 class Diagnostics(BaseAPI):
     """API for diagnostic information:
     http://api.carmd.com/v3.0/diag"""
     api = 'diag'
-
     @api_method
-    def vin(self, vin_no, mileage, dtc):
-        self.vehicle = {
-            'vin': vin_no,
-            'mileage': mileage,
-            'dtc': dtc
-        }
+    def __call__(self, mileage, dtc):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
+        self.vehicle['dtc'] = dtc
 
 
 class UpcomingRepairs(BaseAPI):
     """API for upcoming repairs:
     http://api.carmd.com/v3.0/upcoming"""
     api = 'upcoming'
-
     @api_method
-    def vin(self, vin_no, mileage):
-        self.vehicle = {
-            'vin': vin_no,
-            'mileage': mileage
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model, mileage):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper(),
-            'mileage': mileage
-        }
+    def __call__(self, mileage):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['mileage'] = mileage
 
 
-class TSB(BaseAPI):
-    """API for technical service bulletin:
+class TSBVin(BaseAPI):
+    """API for technical service bulletin using VIN:
     http://api.carmd.com/v3.0/tsb"""
     api = 'tsb'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
+
+class TSBMake(BaseAPI):
+    """API for technical service bulletin using make:
+    http://api.carmd.com/v3.0/tsb"""
+    api = 'tsb'
     @api_method
-    def make(self, year, manufacturer, model, engine):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper(),
-            'engine': engine
-        }
+    def __call__(self, engine):
+        self.vehicle = self.kwargs['vehicle']
+        self.vehicle['engine'] = engine
 
 
 class SafetyRecalls(BaseAPI):
     """API for safety recalls:
     http://api.carmd.com/v3.0/recall"""
     api = 'recall'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper()
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class VehicleWarranty(BaseAPI):
     """API for warranty status:
     http://api.carmd.com/v3.0/warranty"""
     api = 'warranty'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
-
-    @api_method
-    def make(self, year, manufacturer, model):
-        self.vehicle = {
-            'year': year,
-            'make': manufacturer.upper(),
-            'model': model.upper()
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class VehicleImage(BaseAPI):
     """API for getting vehicle image:
     http://api.carmd.com/v3.0/image"""
     api = 'image'
-
     @api_method
-    def vin(self, vin_no):
-        self.vehicle = {
-            'vin': vin_no
-        }
+    def __call__(self):
+        self.vehicle = self.kwargs['vehicle']
 
 
 class YMME(BaseAPI):
@@ -320,44 +237,47 @@ class Credits(BaseAPI):
     """API for account credits:
     http://api.carmd.com/v3.0/credits"""
     api = 'credits'
-
     @api_method
-    def balance(self):
+    def __call__(self):
         self.vehicle = {}
 
 
 class CarMD():
     """General class for the CarMD API"""
-    fields = None
-    decode = None
-    decode_enhanced = None
-    obd2 = None
-    maintenance = None
-    maintenance_list = None
-    repairs = None
-    diagnostics = None
-    future_repairs = None
-    tech_service = None
-    recalls = None
-    warranty = None
-    vehicle_image = None
-    ymme = None
-    acct_credits = None
-
     def __init__(self, auth_key=None, partner_key=None):
-        args = (auth_key, partner_key)
-        self.fields = Fields(*args)
-        self.decode = Decode(*args)
-        self.decode_enhanced = DecodeEnhanced(*args)
-        self.obd2 = OBDPortLocation(*args)
-        self.maintenance = Maintenance(*args)
-        self.maintenance_list = MaintenanceList(*args)
-        self.repairs = Repairs(*args)
-        self.diagnostics = Diagnostics(*args)
-        self.future_repairs = UpcomingRepairs(*args)
-        self.tech_service = TSB(*args)
-        self.recalls = SafetyRecalls(*args)
-        self.warranty = VehicleWarranty(*args)
-        self.vehicle_image = VehicleImage(*args)
-        self.ymme = YMME(*args)
-        self.acct_credits = Credits(*args)
+        self.args = (auth_key, partner_key)
+        self.ymme = YMME(*self.args)
+        self.acct_credits = Credits(*self.args)
+
+    def make(self, year, manufacturer, model):
+        my_vehicle = {
+            'year': year,
+            'make': manufacturer.upper(),
+            'model': model.upper()
+        }
+        self.instanciate(vehicle=my_vehicle)
+
+    def vin(self, vin_no):
+        my_vehicle = {
+            'vin': vin_no}
+        self.instanciate(vehicle=my_vehicle)
+
+    def instanciate(self, **kwargs):
+        if 'vin' in kwargs['vehicle']:
+            self.fields = FieldsVin(*self.args, **kwargs)
+            self.tech_service = TSBVin(*self.args, **kwargs)
+            self.decode = Decode(*self.args, **kwargs)
+            self.decode_enhanced = DecodeEnhanced(*self.args, **kwargs)
+            self.repairs = Repairs(*self.args, **kwargs)
+            self.diagnostics = Diagnostics(*self.args, **kwargs)
+            self.vehicle_image = VehicleImage(*self.args, **kwargs)
+        else:
+            self.fields = FieldsMake(*self.args, **kwargs)
+            self.tech_service = TSBMake(*self.args, **kwargs)
+
+        self.obd2 = OBDPortLocation(*self.args, **kwargs)
+        self.maintenance = Maintenance(*self.args, **kwargs)
+        self.maintenance_list = MaintenanceList(*self.args, **kwargs)
+        self.future_repairs = UpcomingRepairs(*self.args, **kwargs)
+        self.recalls = SafetyRecalls(*self.args, **kwargs)
+        self.warranty = VehicleWarranty(*self.args, **kwargs)
